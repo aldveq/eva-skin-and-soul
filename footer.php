0d24336604069b7d5ab9d82cb0a2d8791c62ef4e
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2023-09-28 14:43:15 -0600

    footer completed

--- footer.php
+++ footer.php
@@ -9,22 +9,68 @@
  * @package Base_Theme
  */
 
+// ACF Variables
+$theme_options_logo_white = get_field('theme_options_logo_white', 'option');
+$theme_options_socials = get_field('theme_options_socials', 'option');
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-eva-skin-and-soul' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'theme-eva-skin-and-soul' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-eva-skin-and-soul' ), 'theme-eva-skin-and-soul', '<a href="https://createape.com/">CreateApe</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="site-footer__wrapper">
+						<?php
+							if (isset($theme_options_logo_white) && !empty($theme_options_logo_white)):
+								?>
+									<a href="<?php echo esc_url(home_url())?>" target="_self">
+									<?php
+									echo wp_get_attachment_image( 
+										$theme_options_logo_white, 
+										'logo_size', 
+										false, 
+										array(
+											'class' => 'logo',
+											'loading' => 'lazy'
+										)
+									);
+									?>
+									</a>
+								<?php
+							endif;
+						?>
+
+						<div class="site-footer__navigation-wrapper">
+						<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'footer-menu',
+									'menu_id'        => 'footer-menu',
+								)
+							);
+						?>
+						</div>
+
+						<div class="site-footer__socials-wrapper">
+							<ul>
+								<?php
+									if(isset($theme_options_socials) 
+									&& is_array($theme_options_socials) 
+									&& count($theme_options_socials) > 0):
+										foreach ($theme_options_socials as $social):
+											$social_url = $social['theme_option_social_link']['url'];
+											$social_target = $social['theme_option_social_link']['target'] ? $social['theme_option_social_link']['target'] : '_self';
+											?>
+											<li><a href="<?php echo esc_url($social_url); ?>" target="<?php echo esc_attr($social_target); ?>"><i class="<?php echo esc_attr( $social['theme_options_fontawesome_icon'] ); ?>"></i></a></li>
+											<?php
+										endforeach;
+									endif;
+								?>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- functions.php
+++ functions.php
@@ -53,6 +53,7 @@ function base_theme_setup()
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__('Primary', 'theme-eva-skin-and-soul'),
+			'footer-menu' => esc_html__('Footer Menu', 'theme-eva-skin-and-soul'),
 		)
 	);
 
