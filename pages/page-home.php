7af0a616db6a040ba0662ddd6e5fe8fc37bc6768
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2023-09-28 13:55:49 -0600

    added extra spacing at the bottom of the page home template

--- pages/page-home.php
+++ pages/page-home.php
@@ -11,4 +11,9 @@ get_template_part('pages/modules/home', 'hero');
 get_template_part('pages/modules/general', 'info');
 get_template_part('pages/modules/services', 'info');
 
+?>
+<!--Spacing-->
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php
+
 get_footer();
